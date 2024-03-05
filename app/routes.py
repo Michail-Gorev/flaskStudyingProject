827de79adc6adba560e06b9088ebc4d258445e1e
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2024-03-05 14:53:31 +0300

    500 error wrapper added

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,11 @@ from flask import make_response
 import app.constants
 
 
+@first_app.errorhandler(500)
+def server_error_return(e):
+    return render_template('500.html'), 500
+
+
 @first_app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404

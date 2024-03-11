d9aa144144ae38785357eea4a4f7b1f13694f294
m.gorev <moisha504@gmail.com>
2024-03-11 13:57:39 +0300

    bootstrap4 wrapper added to app

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap4
 
 import app.constants
 
 first_app = Flask(__name__, static_url_path='/static')
-
+bootstrap = Bootstrap4(first_app)
 from app import routes
 
 first_app.config['SECRET_KEY'] = app.constants.SECRET_KEY
